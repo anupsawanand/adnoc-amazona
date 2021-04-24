3b13a80740df6ecee9ed253ab3de0efe8c5c58f1
anoopsawanand <anupsawanand@gmail.com>
2021-04-24 14:35:31 +0530

    test2

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,7 @@ app.get('*', (req, res) =>
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
 // });
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
