d519ea527d4aac105c78fb511ab56d07125a0925
anoopsawanand <anupsawanand@gmail.com>
2021-04-24 14:08:09 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -36,9 +36,13 @@ app.use((err, req, res, next) => {
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-app.get('/', (req, res) => {
-  res.send('Server is ready');
-});
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
